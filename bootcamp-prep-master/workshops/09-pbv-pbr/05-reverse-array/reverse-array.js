976cedcd06c6f55a06e065b5beda129e4265545b
Sidharth Nambiar <snambiar01@gmail.com>
2021-05-05 01:09:02 -0400

    09 pbv-pbr complete

--- bootcamp-prep-master/workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ bootcamp-prep-master/workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function reverseArray(originalArr) {
+
+    let tempArr = [];
+
+    while (originalArr.length > 0) {
+        tempArr.push(originalArr.pop());
+    }
+
+    while (tempArr.length > 0) {
+        originalArr.push(tempArr.shift());
+    }
+
+    return originalArr;
+
+}
--- bootcamp-prep-master/workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ bootcamp-prep-master/workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+
+function deeperCopy(originalArr) {
+
+    let deepCopiedArray = [];
+
+    for (let i = 0; i < originalArr.length; i++) {
+
+        if (Array.isArray(originalArr[i])) {
+
+            let tempArray = [];
+
+            for (let j = 0; j < originalArr[i].length; j++) {
+                tempArray.push(originalArr[i][j]);
+            }
+            deepCopiedArray.push(tempArray);
+
+
+        }
+        else {
+            deepCopiedArray.push(originalArr[i])
+        }
+    }
+
+    return deepCopiedArray;
+}
